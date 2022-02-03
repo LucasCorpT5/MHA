4498740872a7468adffa33b3b019e65299159c7e
LucasCorpT5 <lucas.contatoestud@gmail.com>
2022-02-03 09:58:25 -0400

    sql_alchemy_create_basic_db_upd6

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ from flask_restful import Resource, Api
 from resources.hotel import Hoteis, Hotel
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///HotelServiceAPI'
 api = Api(app)
+
+@app.before_first_request
+def cria_banco():
+    banco.create_all()
 
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
