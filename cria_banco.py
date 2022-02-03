4a5b42ddf103ca149346439398b11966b8cacb2a
LucasCorpT5 <lucas.contatoestud@gmail.com>
2022-02-02 21:58:27 -0400

    init_model_sqlite_simple_upd3

--- cria_banco.py
+++ cria_banco.py
@@ -7,3 +7,6 @@ cria_tabela = "CREATE TABLE IF NOT EXISTS hoteis (hotel_id text PRIMARY KEY, \
 nome text, estrelas real, diaria real, cidade text)"
 
 cursor.execute(cria_tabela)
+
+connection.commit()
+connection.close()
