9559e446698a5ab968810c582250fb98e5600576
LucasCorpT5 <lucas.contatoestud@gmail.com>
2022-02-03 10:25:08 -0400

    sql_alchemy_create_basic_db_upd9

--- models/hotel.py
+++ models/hotel.py
@@ -5,6 +5,9 @@ class HotelModel(banco.Model):
 
     hotel_id = banco.Column(banco.String, primary_key=True)
     nome = banco.Column(banco.String(80))
+    estrelas = banco.Column(banco.Float(precision=1))
+    diaria = banco.Column(banco.Float(precision=2))
+    cidade = banco.Column(banco.String(40))
 
     def __init__(self, hotel_id, nome, estrelas, diaria, cidade):
         self.hotel_id = hotel_id
