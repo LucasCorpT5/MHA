e1963963e5bf0e4c26076f5be75e3018c12a87b3
LucasCorpT5 <lucas.contatoestud@gmail.com>
2022-02-02 22:10:23 -0400

    init_model_sqlite_simple_upd

--- cria_banco.py
+++ cria_banco.py
@@ -6,7 +6,7 @@ cursor = connection.cursor()
 cria_tabela = "CREATE TABLE IF NOT EXISTS hoteis (hotel_id text PRIMARY KEY, \
 nome text, estrelas real, diaria real, cidade text)"
 
-cria_hotel = "INSERT INTO hoteis VALUES ('alpha', 'Alpha Hotel', 4.3, 345.50, Florianopolis)"
+cria_hotel = "INSERT INTO hoteis VALUES ('alpha', 'Alpha Hotel', 4.3, 345.50, 'Florianopolis')"
 
 cursor.execute(cria_tabela)
 cursor.execute(cria_hotel)
