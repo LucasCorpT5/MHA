d60a0ddbd8ecc1b674f7f7acc3b572f6a73ab3a8
LucasCorpT5 <lucas.contatoestud@gmail.com>
2022-02-04 13:36:03 -0400

    requestmethod_get_all_simple_db_upd2

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,31 +1,6 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 
-hoteis = [
-    {
-    'hotel_id': 'alpha',
-    'nome': 'alpha hotel',
-    'estrelas': 4.3,
-    'diaria': 420.34,
-    'cidade': 'Curitiba'
-    },
-    {
-    'hotel_id': 'esmeralda',
-    'nome': 'esmeralda hotel',
-    'estrelas': 4.5,
-    'diaria': 450.22,
-    'cidade': 'São Paulo'
-    },
-    {
-    'hotel_id': 'reilly',
-    'nome': 'reilly hotel',
-    'estrelas': 3.9,
-    'diaria': 310.44,
-    'cidade': 'Minas Gerais'
-    }
-
-]
-
 class Hoteis(Resource):
     def get(self):
         return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]}
