f6e8cc6c1a781b7bbbbec5b6730f86a28dc8dd6a
LucasCorpT5 <lucas.contatoestud@gmail.com>
2022-02-03 20:38:50 -0400

    requestmethod_get_simple_db_upd3

--- resources/hotel.py
+++ resources/hotel.py
@@ -60,9 +60,8 @@ class Hotel(Resource):
 
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
-        hotel_objeto = HotelModel(hotel_id, **dados)
-        novo_hotel = hotel_objeto.json()
-        hotel = Hotel.find_hotel(hotel_id)
+        
+        hotel = HotelModel(hotel_id, **dados)
         if hotel:
             hotel.update(novo_hotel)
             return novo_hotel, 200 # Ok
