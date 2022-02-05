6494bc130c0e50b265e0c2ae1d1bd5e36c95ed2f
LucasCorpT5 <lucas.contatoestud@gmail.com>
2022-02-04 23:00:39 -0400

    create_usermodel_in_simpledb_upd2

--- models/usuario.py
+++ models/usuario.py
@@ -1,1 +1,14 @@
 from sql_alchemy import banco
+
+class UserModel(banco.Model):
+    __tablename__ = "users"
+
+    user_id = banco.Column(banco.Integer, primary_key=True)
+    nome = banco.Column(banco.String(40))
+    email = banco.Column(banco.String(40))
+    senha = banco.Column(banco.String(40))
+
+    def __init__(self, nome, email, senha):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
