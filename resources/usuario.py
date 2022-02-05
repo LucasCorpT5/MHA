e8bc22345b6b79adc60da6f2da41a4327c4c7739
LucasCorpT5 <lucas.contatoestud@gmail.com>
2022-02-05 18:55:26 -0400

    create_user_register_JWT_upd19

--- resources/usuario.py
+++ resources/usuario.py
@@ -1,6 +1,11 @@
 from flask_restful import Resource, reqparse
 from models.usuario import UserModel
 
+atributos = reqparse.RequestParser()
+atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot be left blank")
+atributos.add_argument('senha', type=str, required=True, help="The field 'senha' cannot be left blank")
+dados = atributos.parse_args()
+
 class User(Resource):
     def get(self, user_id):
         user = UserModel.find_user(user_id)
@@ -19,11 +24,6 @@ class User(Resource):
 
 class UserRegister(Resource):
     def post(self):
-        atributos = reqparse.RequestParser()
-        atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot be left blank")
-        atributos.add_argument('senha', type=str, required=True, help="The field 'senha' cannot be left blank")
-        dados = atributos.parse_args()
-
         if UserModel.find_by_login(dados['login']):
             return {'message': "The login '{}' already exists.".format(dados['login'])}
 
@@ -34,4 +34,4 @@ class UserRegister(Resource):
 class UserLogin(Resource):
     @classmethod
     def post(cls):
-        pass
+        dados = atributos.parse_args()
