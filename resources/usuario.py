27f83804184e5d0e74c6cc8b8c93d15eba449115
LucasCorpT5 <lucas.contatoestud@gmail.com>
2022-02-05 12:10:41 -0400

    create_user_resources_upd5

--- resources/usuario.py
+++ resources/usuario.py
@@ -2,8 +2,12 @@ from flask_restful import Resource, reqparse
 from models.usuario import UserModel
 
 class User(Resource):
-    def get(self):
-        pass
-        
-    def delete(self);
+    def get(self, user_id):
+        user = UserModel.find_user(user_id)
+        if user:
+            return user.json()
+        else:
+            return {'message': 'Hotel not found'}, 404
+
+    def delete(self):
         pass
