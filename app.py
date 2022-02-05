de633cd9c109c28093fbcc5307bf5e5ac36ffae9
LucasCorpT5 <lucas.contatoestud@gmail.com>
2022-02-05 14:13:15 -0400

    create_user_register_JWT_upd11

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from resources.hotel import Hoteis, Hotel
-from resources.usuario import User
+from resources.usuario import User, UserRegister
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///HotelServiceAPI'
@@ -15,6 +15,7 @@ def cria_banco():
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 api.add_resource(User, '/usuarios/<int:user_id>')
+api.add_resource(UserRegister, '/cadastro')
 
 if __name__ == '__main__':
     from sql_alchemy import banco
