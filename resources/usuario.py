46b4a90fc7bd1cafb3a7b5ecc06ebc105301ce29
LucasCorpT5 <lucas.contatoestud@gmail.com>
2022-02-05 20:40:30 -0400

    create_user_register_JWT_upd22

--- resources/usuario.py
+++ resources/usuario.py
@@ -40,4 +40,6 @@ class UserLogin(Resource):
         user = UserModel.find_by_login(dados['login'])
 
         if user and safe_str_cmp(user.senha, dados['senha']):
-            pass
+            token_de_acesso = create_access_token(identity=user.user_id)
+            return {'access_token': token_de_acesso}, 200
+        return {'message': 'The username or password is incorrect'}
