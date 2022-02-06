e26b032c7ca9dfd2ca9293cfa9004faaae336c2d
LucasCorpT5 <lucas.contatoestud@gmail.com>
2022-02-05 22:42:17 -0400

    create_user_register_JWT_upd27

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///HotelServiceAPI'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
+jwt = JWTManager(app)
 
 @app.before_first_request
 def cria_banco():
@@ -20,6 +21,7 @@ api.add_resource(UserRegister, '/cadastro')
 api.add_resource(UserLogin, '/login')
 
 if __name__ == '__main__':
+    app.secret_key = 'secret'
     from sql_alchemy import banco
     banco.init_app(app)
     app.run(debug=True)
