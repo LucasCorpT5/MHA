fd13cb709f5144587c6a29f15f426334ddd05ee8
LucasCorpT5 <lucas.contatoestud@gmail.com>
2022-02-06 21:51:24 -0400

    logged_out_upd9

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def verifica_blacklist(self, token):
 
 @jwt.revoked_token_loader
 def token_de_acesso_invalidado(jwt_header, jwt_payload):
-    return jsonify({'message': ''})
+    return jsonify({'message': 'You have been logged out.'}), 401 # Unauthorized
 
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
