40e27052124ece0952b678a092ff11a1ccafd9b3
LucasCorpT5 <lucas.contatoestud@gmail.com>
2022-02-07 19:13:41 -0400

    filters_for_consult_params_upd6

--- resources/hotel.py
+++ resources/hotel.py
@@ -4,7 +4,16 @@ from flask_jwt_extended import jwt_required
 import sqlite3
 
 def normalize_path_params(cidade=None, estrelas_min=0, estrelas_max=5, diaria_min=0, diaria_max=10000, limit=50, offset=0, **dados):
-    pass
+    if cidade:
+        return {
+            'estrelas_min': estrelas_min,
+            'estrelas_max': estrelas_max,
+            'diaria_min': diaria_min,
+            'diaria_max': diaria_max,
+            'cidade': cidade,
+            'limit': limit,
+            'offset': offset
+        }
 
 path_params = reqparse.RequestParser()
 path_params.add_argument('cidade', type=str)
